061c0b923532460a57e0e6bce813a543e48ecdea
Jacky <newbdez33@gmail.com>
2013-12-09 10:26:08 +0800

    Initial Commit

---
+++ marchinggame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  marchinggameAppController.h
+//  marchinggame
+//
+//  Created by Jack on 9/12/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ marchinggame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  marchinggameAppController.h
+//  marchinggame
+//
+//  Created by Jack on 9/12/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
